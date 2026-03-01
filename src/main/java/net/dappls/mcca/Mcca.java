3a6239ce6e1abffc27cfe07e9150ede65a5f8eb6
dappls <101605544+dappls@users.noreply.github.com>
2026-03-01 11:53:47 -0800

    first commit

---
+++ src/main/java/net/dappls/mcca/Mcca.java
@@ -0,0 +1,12 @@
+package net.dappls.mcca;
+
+import net.fabricmc.api.ModInitializer;
+
+
+public class Mcca implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/net/dappls/mcca/client/MccaClient.java
@@ -0,0 +1,12 @@
+package net.dappls.mcca.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MccaClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+
+}
